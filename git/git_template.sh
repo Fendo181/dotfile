21f1fe1186fb5592eb2028e8513e3689397e68e5
endu <futoshi.endo@pepabo.com>
2018-03-06 14:27:33 +0900

    add git_template.sh
    引数のディレクトリを与えたら、そこでgitに関するテンプレートをコピーするシェルを作成しました。

---
+++ git/git_template.sh
@@ -0,0 +1,12 @@
+cat <<__EOT__
+$1ディレクトリに以下のテンプレートファイルをコピーします！
+.github/ISSUE_TEMPLATE.md
+.github/PULL_REQUEST_TEMPLATE.md
+.commit_template
+__EOT__
+
+cp -r .github $1
+cp .commit_template $1
+# emoji prefix
+cd $1
+git config commit.template .commit_template
